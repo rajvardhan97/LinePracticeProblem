0ce4c778084036a212f67644e95f336f64a94c7e
rajvardhan97 <104343413+rajvardhan97@users.noreply.github.com>
2022-05-11 10:09:12 +0530

    [Raj]Add: UC3- Compare

--- UC3-Compare.cs
+++ UC3-Compare.cs
@@ -1,6 +1,30 @@
-﻿namespace LinePracticeProblem
+namespace LinePracticeProblem
 {
-   
+    public class Line
+    {
+        public int x1 = 20, x2 = 40, y1 = 50, y2 = 80;
+        public double length;
+        public int l1, l2;
+
+        public void Cartesian()
+        {
+            length = Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+            Console.WriteLine(length);
+        }
+
+        public void Equality()
+        {
+            l1 = x2 - x1;
+            l2 = y2 - y1;
+            if(l1.Equals(l2))
+            {
+                Console.WriteLine("The given straight lines are identical");
+            }
+            else
+            {
+                Console.WriteLine("The given straight lines are not identical");
+            }
+        }
 
         public void Compare()
         {
@@ -21,5 +45,14 @@
             }
         }
 
+        public static void Main(string[] args)
+        {
+            Line line = new Line();
+            line.Cartesian();
+            line.Equality();
+            line.Compare();
+
+
+        }
+    }
 }
-  
