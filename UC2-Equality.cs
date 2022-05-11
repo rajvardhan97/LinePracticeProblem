1644f9a7664f98505bff54ef66f29349cb120a9b
rajvardhan97 <104343413+rajvardhan97@users.noreply.github.com>
2022-05-11 08:38:55 +0530

    Check the equality of lines

---
+++ UC2-Equality.cs
@@ -0,0 +1,22 @@
+﻿namespace LinePracticeProblem
+{
+  
+
+        public void Equality()
+        {
+            l1 = x2 - x1;
+            l2 = y2 - y1;
+            if(l1.Equals(l2))
+            {
+                Console.WriteLine("The given straight lines are identical");
+            }
+            else
+            {
+                Console.WriteLine("The given straight lines are not identical");
+            }
+        }
+
+      
+    
+}
+  
