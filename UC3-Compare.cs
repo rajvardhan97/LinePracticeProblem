cf60c0f4685bbba6ee67d8d0f39527563be2a022
rajvardhan97 <104343413+rajvardhan97@users.noreply.github.com>
2022-05-11 08:45:00 +0530

    [Raj]Add: Compare the lines

---
+++ UC3-Compare.cs
@@ -0,0 +1,25 @@
+﻿namespace LinePracticeProblem
+{
+   
+
+        public void Compare()
+        {
+            l1 = x2 - x1;
+            l2 = y2 - y1;
+            int n = (l1.CompareTo(l2));
+            if ( n > 0)
+            {
+                Console.WriteLine("line 1 is Greater than line 2");
+            }
+            else if ( n < 0 )
+            {
+                Console.WriteLine("line 2 is Greater than Line 1");
+            }
+            else 
+            {
+                Console.WriteLine("Line 1 and Line 2 are equal");
+            }
+        }
+
+}
+  
